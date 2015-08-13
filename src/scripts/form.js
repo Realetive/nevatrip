a017909133c0e80ea9c15ac5f81e772205e132fd
Realetive <realetive@yandex.ru>
2015-08-13 09:36:33 +0300

    Updated order form

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -1,6 +1,9 @@
-var sum      = 0,
-    count    = 0,
-    double   = false,
+/* global $, p, ticketPriceDouble */
+'use strict';
+
+var sum = 0,
+    count = 0,
+    double = false,
     discount = 0,
     arrayPriceDouble = [];
 
@@ -8,68 +11,68 @@ var sum      = 0,
  * IE8+ analog of jQuery's $(el).on(eventName, eventHandler)
  * use: addEventListener(el, eventName, handler);
  */
-function addEventListener( el, eventName, handler ) {
-  if ( el.addEventListener ) {
-    el.addEventListener( eventName, handler );
-  } else {
-    el.attachEvent( 'on' + eventName, function() {
-      handler.call( el );
-    });
-  }
-}
-
-var showSum = function(){
-      $Sum.value = sum;
-      $ShowSum.innerHTML = sum;
-    },
-    recalcSum = function(){
-      sum = 0;
-      count = 0;
-      Array.prototype.forEach.call($Tickets, recalcSumByTicket);
-
-      sum = parseInt((sum * (100 - discount)) / 100);
-
-      $($Form).trigger('updateSum');
-      checkCount();
-    },
-    recalcSumByTicket = function(el, i){
-      if(el.value){
-        count += parseInt(el.value);
-      }
-      sum += el.dataset.ticketPrice * el.value;
-    },
-    checkCount = function(){
-      if(count < 1){
-        $CountError.style.display = 'block';
-        $Submit.disabled = true;
-      } else{
-        $CountError.style.display = 'none';
-        $Submit.disabled = false;
-      }
-    }
+// function addEventListener( el, eventName, handler ) {
+//   if ( el.addEventListener ) {
+//     el.addEventListener( eventName, handler );
+//   } else {
+//     el.attachEvent( 'on' + eventName, function() {
+//       handler.call( el );
+//     });
+//   }
+// }
 
 var $Form = document.getElementById( 'BuyForm' );
 if ( $Form != null ) {
 
-  var $Submit        = document.getElementById( 'BuyFormSubmit' )
-    , $CountError    = document.getElementById( 'BuyFormCountError' )
-    , $Sum           = document.getElementById( 'BuyFormSum' )
-    , $ShowSum       = document.getElementById( 'BuyFormShowSum' )
-    , $Total         = document.getElementById( 'BuyFormTotal' )
-    , $TripPoints    = document.getElementById( 'BuyFormTripPoints' )
-    , $Promocode     = document.getElementById( 'BuyFormPromocode' )
-    , $Tickets       = document.querySelectorAll( '.buy-form-ticket' )
+  var $Submit = document.getElementById( 'BuyFormSubmit' )
+    , $CountError = document.getElementById( 'BuyFormCountError' )
+    , $Sum = document.getElementById( 'BuyFormSum' )
+    , $ShowSum = document.getElementById( 'BuyFormShowSum' )
+    , $Total = document.getElementById( 'BuyFormTotal' )
+    , $TripPoints = document.getElementById( 'BuyFormTripPoints' )
+    , $Promocode = document.getElementById( 'BuyFormPromocode' )
+    , $Tickets = document.querySelectorAll( '.buy-form-ticket' )
     , $TicketsPrices = document.querySelectorAll( '.buy-form-ticket-price' )
-    , $TicketsInput  = document.querySelectorAll( '.buy-form-ticket-input' )
-    , $TicketsMinus  = document.querySelectorAll( '.buy-form-ticket-minus' )
-    , $TicketsPlus   = document.querySelectorAll( '.buy-form-ticket-plus' );
-  
+    , $TicketsInput = document.querySelectorAll( '.buy-form-ticket-input' )
+    , $TicketsMinus = document.querySelectorAll( '.buy-form-ticket-minus' )
+    , $TicketsPlus = document.querySelectorAll( '.buy-form-ticket-plus' );
+
+  var showSum = function(){
+        $Sum.value = sum;
+        $ShowSum.innerHTML = sum;
+      },
+      recalcSumByTicket = function( el ){
+        if( el.value ){
+          count += parseInt( el.value );
+        }
+        sum += el.dataset.ticketPrice * el.value;
+      },
+      checkCount = function(){
+        if( count < 1 ) {
+          $CountError.style.display = 'block';
+          $Submit.disabled = true;
+        } else {
+          $CountError.style.display = 'none';
+          $Submit.disabled = false;
+        }
+      },
+      recalcSum = function(){
+        sum = 0;
+        count = 0;
+        Array.prototype.forEach.call( $Tickets, recalcSumByTicket);
+
+        sum = parseInt((sum * (100 - discount)) / 100);
+
+        $( $Form).trigger('updateSum');
+        checkCount();
+      };
+
   $Total.style.display = 'block';
 
   if ( window.ticketPriceDouble !== undefined ) {
     arrayPriceDouble = ticketPriceDouble.split(',');
   }
-  
+
   if ( window.p !== undefined ) {
     $( $Promocode ).on( 'change keyup', function(){
       var pp = this.value.toLowerCase();
@@ -80,58 +83,58 @@ if ( $Form != null ) {
     });
   }
 
-  $($Form).on('updateSum', showSum);
-  
-  $($Tickets).on('change keyup', function(){
-    if(isNaN(this.value)){
+  $( $Form ).on( 'updateSum', showSum );
+
+  $( $Tickets ).on( 'change keyup', function() {
+    if( isNaN( this.value ) ){
       this.value = 0;
     } else{
-      this.value = parseInt(this.value);
+      this.value = parseInt( this.value );
     }
     recalcSum();
   });
-  
-  $($TicketsMinus).on('click', function(){
+
+  $( $TicketsMinus ).on( 'click', function() {
     var currentTicket = this.nextElementSibling;
-    if(currentTicket.value >= 1){
+    if( currentTicket.value >= 1 ){
       currentTicket.value--;
     }
     recalcSum();
   });
-  
-  $($TicketsPlus).on('click', function(){
+
+  $( $TicketsPlus ).on( 'click', function() {
     var currentTicket = this.previousElementSibling;
     currentTicket.value++;
     recalcSum();
   });
-  
-  $($TripPoints).on('change', function(){
+
+  $( $TripPoints ).on( 'change', function(){
     var c = this.options.length;
     var o = this.options.selectedIndex;
-    if( (c == ( o + 1 ) ) && !double ){
+    if( ( c === ( o + 1 ) ) && !double ){
       Array.prototype.forEach.call( $Tickets, function( el, i ) {
         var newPrice = arrayPriceDouble[ i ] > 0 ? arrayPriceDouble[ i ] : el.dataset.ticketPrice;
         el.dataset.originalTicketPrice = el.dataset.ticketPrice;
         el.dataset.ticketPrice = newPrice;
       });
-      Array.prototype.forEach.call($TicketsPrices, function(el, i){
+      Array.prototype.forEach.call( $TicketsPrices, function( el, i ){
         var newPrice = arrayPriceDouble[ i ] > 0 ? arrayPriceDouble[ i ] : el.dataset.ticketPrice;
         el.innerHTML = newPrice;
       });
-      Array.prototype.forEach.call($TicketsInput, function(el, i){
+      Array.prototype.forEach.call( $TicketsInput, function( el, i ){
         var newPrice = arrayPriceDouble[ i ] > 0 ? arrayPriceDouble[ i ] : el.dataset.ticketPrice;
         el.value = newPrice;
       });
       double = true;
       recalcSum();
     } else if( double ) {
-      Array.prototype.forEach.call($Tickets, function(el ,i){
+      Array.prototype.forEach.call( $Tickets, function( el ){
         el.dataset.ticketPrice = el.dataset.originalTicketPrice;
       });
-      Array.prototype.forEach.call($TicketsPrices, function(el ,i){
+      Array.prototype.forEach.call( $TicketsPrices, function( el ){
         el.innerHTML = el.dataset.ticketPrice;
       });
-      Array.prototype.forEach.call($TicketsInput, function(el ,i){
+      Array.prototype.forEach.call( $TicketsInput, function( el ){
         el.value = el.dataset.ticketPrice;
       });
       double = false;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,26 @@
+/* eslint new-cap: [ 1, { capIsNewExceptions: [ "Gallery" ] } ] */
+/* global $, blueimp */
 'use strict';
 
 $( '[data-toggle="tooltip"]' ).tooltip();
+$( '[data-toggle="popover"]' ).popover();
 
-document.getElementById('links').onclick = function (event) {
+$('.input-group.date').datepicker({
+  weekStart: 1,
+  language: 'ru',
+  keyboardNavigation: false,
+  todayHighlight: false,
+  autoclose: true
+});
+
+var galleryContainer = document.getElementById( 'gallery' );
+if ( galleryContainer != null ) {
+  galleryContainer.onclick = function ( event ) {
     event = event || window.event;
     var target = event.target || event.srcElement,
-        link = target.src ? target.parentNode : target,
-        options = {index: link, event: event},
-        links = this.getElementsByTagName('a');
-    blueimp.Gallery(links, options);
-};
+    link = target.src ? target.parentNode : target,
+    options = { index: link, event: event },
+    links = this.getElementsByTagName( 'a' );
+    blueimp.Gallery( links, options );
+  };
+}
